14f2d70ec840ac3c204dea89dc529500d08a647d
Cloud Cheng <annie22336699@hotmail.com>
2022-01-26 11:08:23 +0800

    package.json新增start+dev

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ app.listen(port, ()=>{
     console.log(`server started: ${port} - `, new Date());
     console.log(`http://localhost:${port}`);
 })
+
+// 因為package.json無法註解所以寫在這裡
+// 總之就是在debug那邊新增start及dev語法(?)
+// 可以使用「npm start」「npm test」可以自動執行該行內容
+// 但是dev需要+run：「npm run dev」
