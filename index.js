9d19202aee4e996f270acbd7050cfc8eafc672eb
Cloud Cheng <annie22336699@hotmail.com>
2022-01-26 11:29:39 +0800

    開發端及正式端切換裝置add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log(process.env.NODE_ENV);
+
 require('dotenv').config();
 const express=require('express');
 
@@ -17,3 +19,5 @@ app.listen(port, ()=>{
 // 總之就是在debug那邊新增start及dev語法(?)
 // 可以使用「npm start」「npm test」可以自動執行該行內容
 // 但是dev需要+run：「npm run dev」
+
+// 新增「SET NODE_ENV="       "」，卡開發端或者正式端，方便在開發時候做切換查看，可以載入各種OS系統(寫法不同的因故)，以&&做連接，也可以寫不同的.env檔案進行仔載入
