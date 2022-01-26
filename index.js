5864487301833b5b0c9f5240927bea5af5d02c8f
Cloud Cheng <annie22336699@hotmail.com>
2022-01-26 10:49:35 +0800

    修改index.js 新增pork端口

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require('express');
 
 const app=express();
@@ -6,7 +7,8 @@ app.get('/',(req, res)=>{
     res.send('<h2>Hello</h2>');
 })
 
-app.listen(3000, ()=>{
-    console.log('server started');
-    console.log('http://localhost:3000');
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>{
+    console.log(`server started: ${port} - `, new Date());
+    console.log(`http://localhost:${port}`);
 })
